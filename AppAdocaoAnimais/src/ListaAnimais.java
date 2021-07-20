961123052b515c1b89bb1946e7253e22d89f6fdd
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2021-07-20 15:50:37 -0300

    lista

--- AppAdocaoAnimais/src/ListaAnimais.java
+++ AppAdocaoAnimais/src/ListaAnimais.java
@@ -1,5 +1,12 @@
-
+import java.util.ArrayList;
 import java.util.List;
 public class ListaAnimais {
-
+	
+	private List<Animal> animais;
+	
+	public ListaAnimais() {
+		animais=new ArrayList<Animal>();
+	}
+	
+	
 }
