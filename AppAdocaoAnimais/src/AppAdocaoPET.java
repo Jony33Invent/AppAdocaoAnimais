598a3ad6646da7677674cfb34ce125c353237337
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2021-07-20 15:20:40 -0300

    base do programa java

--- AppAdocaoAnimais/src/AppAdocaoPET.java
+++ AppAdocaoAnimais/src/AppAdocaoPET.java
@@ -1,9 +1,9 @@
 
-public class AppAdocaoAnimais {
+public class AppAdocaoPET {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("teste");
 	}
 
 }
