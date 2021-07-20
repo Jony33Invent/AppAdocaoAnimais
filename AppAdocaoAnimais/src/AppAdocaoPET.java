8427cb00bde427973d87bb540d739d651b103fcb
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2021-07-20 16:02:13 -0300

    interface

--- AppAdocaoAnimais/src/AppAdocaoPET.java
+++ AppAdocaoAnimais/src/AppAdocaoPET.java
@@ -2,8 +2,8 @@
 public class AppAdocaoPET {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("teste");
+		Interface tela=new Interface();
+		tela.Exibe();
 	}
 
 }
---
+++ AppAdocaoAnimais/src/Interface.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+public class Interface extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5529336786288984928L;
+
+	public Interface() {
+		 super("hello world");
+	}
+	
+	public void Exibe() {
+		setSize(400,200);
+		setVisible(true);
+	}
+}
